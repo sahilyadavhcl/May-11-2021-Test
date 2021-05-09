db98e89d394402431dc2ff4ced1386907dcc195a
Sahil Yadav <sahil.yadav@hcl.com>
2021-05-09 17:16:45 -0400

    May-11-2021Test

---
+++ May-11-2021 Test/Excercises/Excercises/Linux/findtext.sh
@@ -0,0 +1,11 @@
+echo "Enter a pattern to search in file: "
+read patt
+grep -q "$patt" file1.txt  2>/dev/null
+ret=$?
+if [ $ret -ne 0 ]; then 
+	echo "not there"
+else
+	echo "found it"
+fi
+exit $ret
+
